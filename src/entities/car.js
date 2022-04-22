1e534826e62ab5607abd6be08d2ac56542a7a414
Lucas Silva Souza <lucassilvadonato23@gmail.com>
2022-04-21 22:57:05 -0300

    car, carcategory, customer classes created

---
+++ src/entities/car.js
@@ -0,0 +1,13 @@
+const Base = require("./base/base");
+
+class Car extends Base {
+    constructor({ id, name, releaseYear, avaliable, gasAvailable }) {
+        super({ id, name })
+
+        this.releaseYear = releaseYear
+        this.avaliable = avaliable
+        this.gasAvailable = gasAvailable
+    }
+}
+
+module.exports = Car
